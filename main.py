e68fccf0c38415dab1fe9fd055efd710b193813c
ThangDuong59 <60088921+ThangDuong59@users.noreply.github.com>
2021-12-22 07:29:16 +0700

    initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+from typing import ContextManager
+from flask import Flask, render_template, request
+from transformers import pipeline
+
+app = Flask(__name__)
+
+model_checkpoint = "nguyenvulebinh/vi-mrc-base"
+nlp = pipeline('question-answering', model=model_checkpoint,
+                tokenizer=model_checkpoint)
+
+@app.route("/")
+def home():
+    return render_template("template.html")
+
+def qa_answer(question, context):
+    QA_input = {
+        "question": question,
+        "context": context 
+    }
+    answer = nlp(QA_input)
+
+    return answer
+    
+@app.route('/question_answering_test', methods=["GET", "POST"])
+def question_answering_test():
+    if request.method == "POST":
+        user_question = request.form["user-question"]
+        user_context = request.form["context"]
+
+        answer = qa_answer(user_question, user_context)
+        print(answer)
+        return render_template("template.html", user_question=user_question, user_context=user_context, answer_found=answer["answer"])
+    else:
+        return ("nothing")
+    
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=8080, debug=True)
